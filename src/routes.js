b7a70b18c6d25e7a23e4aa99ab62d86d24fa3a95
Michael Prather <mprather1@gmail.com>
2017-04-05 00:33:37 +0000

    refactored router

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,27 @@
 import express from 'express'
 import {models} from './queries'
 
-const router = express.Router()
-
-router.route('/models')
-  .get(models.getAllModels)
-  .post(models.createModel)
-
-router.route('/models/:id')
-  .get(models.getSingleModel)
-  .put(models.updateSingleModel)
-  .delete(models.removeModel)
-
-export default router
+export default function getRouter (options) {
+  const router = express.Router()
+  
+  router.route('/models')
+    .get(function (req, res) {
+      models.getAllModels(req, res, null, options)
+    })
+    .post(function (req, res) {
+      models.createModel(req, res, null, options)
+    })
+  
+  router.route('/models/:id')
+    .get(function (req, res) {
+      models.getSingleModel(req, res, null, options)
+    })
+    .put(function (req, res) {
+      models.updateSingleModel(req, res, null,  options)
+    })
+    .delete(function (req, res) {
+      models.removeModel(req, res, null, options)
+    })
+ 
+ return router 
+}
--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
-import router from './routes'
+import getRouter from './routes'
 import {Server} from 'http'
 import chalk from 'chalk'
-import logger from 'winston-color'
+import winston from 'winston-color'
 
 const options = {
   app: express(),
   port: process.env.PORT || 8000,
-  environment: process.env.NODE_ENV || 'development'
+  environment: process.env.NODE_ENV || 'development',
+  logger: winston
 }
 
-const { app, environment, port } = options
+const { app, environment, port, logger } = options
 
 if (environment === 'development') {
   app.use(morgan('dev'))
@@ -21,10 +22,12 @@ if (environment === 'development') {
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+const router = getRouter(options)
+
 app.use('/api', router)
 
 const server = Server(app).listen(port, () => {
-  logger.info(chalk.cyan('listening on', port))
+  logger.info(chalk.bgBlack.magenta('Listening on port', port + '...'))
 })
 
 export default server
