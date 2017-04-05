03e8c7d9634446ea0976f864324d7581744b0567
Michael Prather <mprather1@gmail.com>
2017-04-05 02:10:27 +0000

    next in router

--- src/routes.js
+++ src/routes.js
@@ -5,22 +5,22 @@ export default function getRouter (options) {
   const router = express.Router()
   
   router.route('/models')
-    .get(function (req, res) {
-      models.getAllModels(req, res, null, options)
+    .get(function (req, res, next) {
+      models.getAllModels(req, res, next, options)
     })
-    .post(function (req, res) {
-      models.createModel(req, res, null, options)
+    .post(function (req, res, next) {
+      models.createModel(req, res, next, options)
     })
   
   router.route('/models/:id')
-    .get(function (req, res) {
-      models.getSingleModel(req, res, null, options)
+    .get(function (req, res, next) {
+      models.getSingleModel(req, res, next, options)
     })
-    .put(function (req, res) {
-      models.updateSingleModel(req, res, null,  options)
+    .put(function (req, res, next) {
+      models.updateSingleModel(req, res, next,  options)
     })
-    .delete(function (req, res) {
-      models.removeModel(req, res, null, options)
+    .delete(function (req, res, next) {
+      models.removeModel(req, res, next, options)
     })
  
  return router 
