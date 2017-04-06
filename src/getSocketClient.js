da13c7b712731f1207f94cd249c5232b084ba7a3
Michael Prather <mprather1@gmail.com>
2017-04-06 00:57:26 +0000

    0.0.5 -> socket client

---
+++ src/getSocketClient.js
@@ -0,0 +1,30 @@
+import net from 'net'
+import DuplexEmitter from 'duplex-emitter'
+import logger from 'winston-color'
+import chalk from 'chalk'
+
+export default function getSocketClient (options) {
+  
+  const {socketPort, hostname, packageName} = options
+  
+  const s = new net.Socket()
+  
+  const socket = s.connect(socketPort, hostname)
+  
+  const emitter = DuplexEmitter(socket)
+
+  socket.on('connect', () => {
+    emitter.emit('connected', packageName)
+  })
+  
+  emitter.once('connected', (data) => {
+    logger.info(`connected to ${chalk.bgBlack.green(data)}...`)
+    socket.on('close', () => {
+      handleClose(data, data)
+    })    
+  })
+  
+  function handleClose (name) {
+    logger.info(`connection to ${chalk.bgBlack.green(name)} closed...`)
+  }  
+}
--- src/server.js
+++ src/server.js
@@ -2,18 +2,23 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import getRouter from './routes'
+import getSocketClient from './getSocketClient'
 import {Server} from 'http'
 import chalk from 'chalk'
 import winston from 'winston-color'
+import pkg from '../package.json'
 
 const options = {
   app: express(),
   port: process.env.PORT || 8000,
+  socketPort: process.env.SOCKET_PORT,
   environment: process.env.NODE_ENV || 'development',
-  logger: winston
+  logger: winston,
+  hostname: process.env.HOSTNAME,
+  packageName: pkg.name
 }
 
-const { app, environment, port, logger } = options
+const { app, environment, port, logger, packageName } = options
 
 if (environment === 'development') {
   app.use(morgan('dev'))
@@ -22,12 +27,13 @@ if (environment === 'development') {
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+const socketClient = getSocketClient(options)
 const router = getRouter(options)
 
 app.use('/api', router)
 
 const server = Server(app).listen(port, () => {
-  logger.info(chalk.bgBlack.magenta('Listening on port', port + '...'))
+  logger.info(`${chalk.bgBlack.cyan(packageName)} listening on port ${port}...`)
 })
 
 export default server
