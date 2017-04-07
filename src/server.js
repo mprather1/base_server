284eb1ffde33150ea0aca0bf7ae17a54883c0c7e
Michael Prather <mprather1@gmail.com>
2017-04-07 02:03:00 +0000

    1.0.0 -> removed socket client

--- src/server.js
+++ src/server.js
@@ -2,32 +2,23 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import getRouter from './routes'
-import getSocketClient from './getSocketClient'
 import {Server} from 'http'
 import chalk from 'chalk'
 import winston from 'winston-color'
 import pkg from '../package.json'
 
-var nextId = 0
-
 const options = {
   app: express(),
   port: process.env.PORT || 8000,
-  socketPort: process.env.SOCKET_PORT,
   environment: process.env.NODE_ENV || 'development',
   logger: winston,
-  hostname: process.env.HOSTNAME,
   packageName: pkg.name,
-  emitters: {},
-  id: ++nextId
 }
-const emitter = getSocketClient(options)
+
 const router = getRouter(options)
-const { app, environment, port, logger, packageName, emitters, id} = options
+const { app, environment, port, logger, packageName} = options
 
-emitters[id] = emitter
-
-if (environment === 'development') {
+if (environment !== 'test') {
   app.use(morgan('dev'))
 }
 
